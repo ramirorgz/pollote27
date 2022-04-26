a8da18ef6a35d5126a0ef72b2ca6a0c26a6c9095
Ray27 <rr16121993@gmail.com>
2022-04-26 01:51:20 -0300

    Coments

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const navToggle = document.querySelector(".nav-toggler");
 const navMenu = document.querySelector(".navbar-nav");
 
+///////// CUANDO MENU ESTE ABIERTO, COLOCAR ARIA-LABEL "CERRAR MENU"; DE LO CONTRARIO "ABRIR MENU"/////
 navToggle.addEventListener("click", () => {
   navMenu.classList.toggle("navbar-nav_visible");
   if (navMenu.classList.contains("nav-menu_visible")) {
