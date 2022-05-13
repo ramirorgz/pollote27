f627f4de9236c710232e5b3b703a6565aa1b68ec
Ray27 <rr16121993@gmail.com>
2022-05-12 21:24:41 -0300

    Fetch

--- index.js
+++ index.js
@@ -3,20 +3,15 @@ VARIABLE */
 const navToggle = document.querySelector(".navbar-toggler");
 const navMenu = document.querySelector(".navbar-nav");
 let listaClientes = [];
-let btnGuardar = document.getElementById("btnGuardar");
+let btnGuardar = document.querySelector("#btnGuardar");
+const btnComprar = document.querySelectorAll(".btnComprar");
+const containerData = document.querySelector(".containerDatos");
+/* CUANDO SE COMPLETE FORMULARIO, ENVIAR EL PEDIDO DESEADO AL LOCAL*/
+const btnPedidoRealizado = document.querySelector("#enviarPedido");
 
-navToggle.addEventListener("click", () => {
-  navMenu.classList.toggle("navbar-nav_visible");
-  ///////// CUANDO MENU ESTE ABIERTO, COLOCAR ARIA-LABEL "CERRAR MENU"; DE LO CONTRARIO "ABRIR MENU"/////
-
-  if (navMenu.classList.contains("navbar-nav_visible")) {
-    navToggle.setAttribute("aria-label", "CerrarMenu");
-  } else {
-    navToggle.setAttribute("aria-label", "AbrirMenu");
-  }
-});
 /* MAIN */
-/* CREO EL OBJETO */
+
+/* CREA EL CLIENTE */
 class Cliente {
   constructor(nombre, domicilio, telefono, pago) {
     this.nombre = nombre;
@@ -25,7 +20,7 @@ class Cliente {
     this.pago = pago;
   }
 }
-
+/* CREA ARRAY DE PRODUCTOS */
 let promos = [
   { nombre: "promo1", precio: 1300 },
   { nombre: "promo2", precio: 1500 },
@@ -50,11 +45,13 @@ let cheff = [
   { nombre: "tortilla", precio: 700 },
   { nombre: "fritas", precio: 500 },
 ];
+
 /* ESTE ES EL MENU COMPLETO */
 let menuCompleto = pollos.concat(sanguches, cheff);
+
 /* FUNCIONES */
 
-/*  */
+/* FORMULARIO */
 
 const crearCLiente = () => {
   let nombre = document.querySelector("#nombre").value;
@@ -64,7 +61,8 @@ const crearCLiente = () => {
 
   const nuevoCliente = new Cliente(nombre, domicilio, telefono, pago);
   console.log(nuevoCliente);
-  /* GUARDANDO CLIENTES EN LA LISTA */
+
+  /* GUARDA CLIENTES EN LA LISTA */
   let listaClientes2 = [];
   if (localStorage.getItem("Clientes") != null) {
     listaClientes2 = JSON.parse(localStorage.getItem("Clientes"));
@@ -94,8 +92,45 @@ const guardar = () => {
     localStorage.setItem("Residentes", JSON.stringify(listaClientes));
   }
 };
-
+/* TRAIGO LOS CLIENTES DESDE EL ARRAY JSON */
+const datosClientes = () => {
+  fetch("clientes.json")
+    .then((response) => response.json())
+    .then((resultado) => {
+      let data = JSON.parse(resultado);
+      console.log(data);
+      /*       data.forEach((cliente) => {
+        containerData.innerHTML += `
+            <h4>${cliente.nombre}</h4>
+            <p>${cliente.domicilio}</p>
+            <p>${cliente.telefono}</p>
+        `;
+      }); */
+    })
+    .catch(
+      (error) => console.log(error)
+      /* (containerData.innerHTML += `<div>Por favor, ingrese sus datos para enviarle el pedido.</div>`) */
+    );
+};
 /* EVENTOS */
+/* CLICK EN COMPRAR, ABRIR FORMULARIO */
+
+navToggle.addEventListener("click", () => {
+  navMenu.classList.toggle("navbar-nav_visible");
+  ///////// CUANDO MENU ESTE ABIERTO, COLOCAR ARIA-LABEL "CERRAR MENU"; DE LO CONTRARIO "ABRIR MENU"/////
+
+  if (navMenu.classList.contains("navbar-nav_visible")) {
+    navToggle.setAttribute("aria-label", "CerrarMenu");
+  } else {
+    navToggle.setAttribute("aria-label", "AbrirMenu");
+  }
+});
+
+btnComprar.onclick = (e) => {
+  e.preventDefault();
+  datosClientes();
+};
+
 btnGuardar.addEventListener("click", (e) => {
   e.preventDefault();
   guardar();
@@ -106,3 +141,6 @@ btnGuardar.addEventListener("click", (e) => {
 console.log(
   localStorage.getItem("Clientes") || "Estamos esperando el primer cliente..."
 );
+
+/* QUE LOS DATOS DEL FORMULARIO JUNTO CON EL PRODUCTO ELEGIDO SE IMPRIMA EN UN DIV, MODO TEXTO */
+/* ENVIAR ESE DIV POR WHATSAPP O MAIL */
